4f26f7931923e6e92c4ca68063d1cc0a7e03b404
bmatis <bmatis@mac.com>
2018-03-30 15:36:37 -0700

    Initial setup and data models.

---
+++ main/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from main.models import Pie, Game
+
+admin.site.register(Pie)
+admin.site.register(Game)
---
+++ main/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Pie(models.Model):
+    """Data model for Pies/Snacks"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text
+
+class Game(models.Model):
+    """Data model for Games."""
+    title = models.CharField(max_length=200)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    gamemaster = models.CharField(max_length=200)
+    system = models.CharField(max_length=200)
+    num_players = models.CharField(max_length=6)
+    length = models.CharField(max_length=2)
+    description = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.title
