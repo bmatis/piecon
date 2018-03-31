f958e4bba42be7f117dac61fdf747aed3aaba9b8
bmatis <bmatis@mac.com>
2018-03-31 15:21:43 -0700

    Built out basic structure and simple pages for viewing all games and pies.

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,21 @@ from django.urls import path
 
 from . import views
 
+app_name = 'main'
+
 urlpatterns = [
     # Home page
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+
+    # Page for showing all games.
+    path('games/', views.games, name='games'),
+
+    # Page for showing all pies.
+    path('pies/', views.pies, name='pies'),
+
+    # About page
+    path('about/', views.about, name='about'),
+
+    # Volunteer page
+    path('volunteer/', views.volunteer, name='volunteer'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
+from .models import Pie
+from .models import Game
+
+current_year = 2018
 
 def index(request):
     """The home page for the PieCon site."""
     return render(request, 'main/index.html')
+
+def games(request):
+    """Page for showing all games for the current year's PieCon."""
+    games = Game.objects.filter(date_added__year=current_year).order_by('-date_added')
+    context = {'games': games}
+    return render(request, 'main/games.html', context)
+
+def pies(request):
+    """Page for showing all pies for the current year's PieCon."""
+    pies = Pie.objects.filter(date_added__year=current_year).order_by('-date_added')
+    context = {'pies': pies}
+    return render(request, 'main/pies.html', context)
+
+def about(request):
+    """The about page for the PieCon site."""
+    return render(request, 'main/about.html')
+
+def volunteer(request):
+    """The volunteer page for the PieCon site."""
+    return render(request, 'main/volunteer.html')
