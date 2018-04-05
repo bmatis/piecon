b09148e7dbcc75674ab846b0cfc10a594ffb9543
bmatis <bmatis@mac.com>
2018-04-04 21:16:28 -0700

    Updated display of Pies and Pie list in admin tool for more usability.

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from main.models import Pie, Game
 
-admin.site.register(Pie)
+class PieAdmin(admin.ModelAdmin):
+    fields = ['text', 'owner', 'date_added']
+    list_display = ('text', 'owner', 'date_added')
+    list_filter = ['date_added']
+    date_hierarchy = 'date_added'
+    ordering = ['-date_added']
+
+admin.site.register(Pie, PieAdmin)
 admin.site.register(Game)
