ac39ec55bd9c5c9f1ca3d78782612491dfe7348b
bmatis <bmatis@mac.com>
2018-04-07 21:08:48 -0700

    Added some unit tests for new account creation page access.

--- users/tests.py
+++ users/tests.py
@@ -46,3 +46,33 @@ class SettingsPageTests(TestCase):
 
         # Expect successful response.
         self.assertEqual(response.status_code, 200)
+
+class NewAccountPageTests(TestCase):
+    def test_logged_out_user_can_access_new_account_page(self):
+        """
+        A visitor that is not logged in should be able to access the account
+        registration page.
+        """
+        # Attempt to get to settings page.
+        desired_url = reverse('users:new_account')
+        response = self.client.get(desired_url)
+
+        # Expect successful response.
+        self.assertEqual(response.status_code, 200)
+
+    def test_logged_in_user_cannot_access_new_account_page(self):
+        """"
+        A user that is logged in cannot access the new account page and is
+        instead redirected to the index page.
+        """
+        # Create test user and login as them.
+        createTestUser(username='testuser')
+        login = self.client.login(username='testuser', password='12345')
+
+        # Attempt to get to new account creation page.
+        desired_url = reverse('users:new_account')
+        response = self.client.get(desired_url)
+
+        # Expect to be redirected to index page instead.
+        expected_redirect = reverse('main:index')
+        self.assertRedirects(response, expected_redirect, 302)
