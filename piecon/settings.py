3ae0aa37005060f94744c408e0a34790fb551987
bmatis <bmatis@mac.com>
2018-04-10 11:22:16 -0700

    Added Heroku production settings.

--- piecon/settings.py
+++ piecon/settings.py
@@ -136,3 +136,31 @@ LOGIN_URL = '/users/login/'
 BOOTSTRAP3 = {
     'include_jquery': True,
 }
+
+# Heroku settings for production
+cwd = os.getcwd()
+if cwd == '/app' or cwd[:4] == '/tmp':
+    import dj_database_url
+
+    DATABASES = {
+            'default': dj_database_url.config(default='postgres://localhost')
+        }
+
+    # Redirect http requests to https
+    SECURE_SSL_REDIRECT =  True
+
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure().
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+    # Only allow heroku to host the project.
+    ALLOWED_HOSTS = ['*']
+    DEBUG = False
+
+    # Static asset configuration
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
+
+    STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
