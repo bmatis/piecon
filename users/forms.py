7183f33df47c5317e9cfb986962940866f66d4b4
bmatis <bmatis@mac.com>
2019-01-29 11:37:09 -0800

    added unique email address restriction to signup

--- users/forms.py
+++ users/forms.py
@@ -3,8 +3,17 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(max_length=254, help_text='Please provide a valid email address.')
+    email = forms.EmailField(max_length=254,
+        help_text='Please provide a valid email address.',
+        error_messages={'exists':
+            'Already exists, please use a different email address.'})
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2', )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError(self.fields['email'].error_messages['exists'])
+        return email
