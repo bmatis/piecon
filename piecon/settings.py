3036a36203ec3769d4d5b1114abdfca84c0aa4ac
bmatis <bmatis@mac.com>
2019-02-07 16:54:13 -0800

    remove old email config

--- piecon/settings.py
+++ piecon/settings.py
@@ -168,12 +168,6 @@ if cwd == '/app' or cwd[:4] == '/tmp':
     STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
     # Production email settings
-    # EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
-    # EMAIL_HOST= 'smtp.sendgrid.net'
-    # EMAIL_PORT = 587
-    # EMAIL_USE_TLS = True
-    # EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
-
     EMAIL_HOST = os.environ.get('MAILGUN_SMTP_SERVER', '')
     EMAIL_PORT = os.environ.get('MAILGUN_SMTP_PORT', '')
     EMAIL_HOST_USER = os.environ.get('MAILGUN_SMTP_LOGIN', '')
